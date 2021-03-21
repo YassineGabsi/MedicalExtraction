640ab2ffdbb28a6c898cfadfe5c3d56023c3f8fa
PsycoCast <gabsiyassine@gmail.com>
2021-03-21 15:30:27 +0100

    register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {LoginComponent} from './pages/login/login.component';
 import {UploadFileComponent} from './pages/upload-file/upload-file.component';
+import {RegisterComponent} from './pages/register/register.component';
 
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
   {
     path: 'upload-file',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { TopSectionComponent } from './pages/home/top-section/top-section.component';
 import { BottomSectionComponent } from './pages/home/bottom-section/bottom-section.component';
 import { UploadFileComponent } from './pages/upload-file/upload-file.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { UploadFileComponent } from './pages/upload-file/upload-file.component';
     HomeComponent,
     TopSectionComponent,
     BottomSectionComponent,
-    UploadFileComponent
+    UploadFileComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
