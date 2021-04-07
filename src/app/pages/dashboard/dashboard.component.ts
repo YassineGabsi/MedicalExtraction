a875b2d410870449aa9f980ca556fa46222b073f
PsycoCast <gabsiyassine@gmail.com>
2021-04-07 19:25:07 +0100

    dashboard content

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -14,12 +14,19 @@ export class DashboardComponent implements OnInit {
   public records = new Array(15);
   public recordSelected = this.records[0];
 
+  public medicalTags = [];
+
   constructor() { }
 
   ngOnInit() {
     for (let i = 0; i < 16; i++) {
       this.records[i] = i + 1;
     }
+
+    this.medicalTags.push('complications');
+    this.medicalTags.push('heart diseases');
+    this.medicalTags.push('coronary artery diseases');
+    this.medicalTags.push('lorem upsum lorem upsum lorem upsum');
   }
 
   selectRecord(i): void {
