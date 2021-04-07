597b71c19a9da92729a5f4eaa89fa0456ac3df99
PsycoCast <gabsiyassine@gmail.com>
2021-04-07 16:56:42 +0100

    configure records sidenav

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -11,14 +11,23 @@ export class DashboardComponent implements OnInit {
   public minimized = false;
   public mode = 'push';
 
+  public records = new Array(15);
+  public recordSelected = this.records[0];
+
   constructor() { }
 
   ngOnInit() {
+    for (let i = 0; i < 16; i++) {
+      this.records[i] = i + 1;
+    }
+  }
+
+  selectRecord(i): void {
+    this.recordSelected = i;
   }
 
   public _toggleSidebar(): void {
     this.opened = !this.opened;
     this.minimized = !this.minimized;
   }
-
 }
