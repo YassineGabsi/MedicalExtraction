bd56d95a07c87e1abd3933622dba75486a9cb944
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-05-06 00:23:22 +0100

    Fix spacy

--- server/model/custom_ner/extract_terms.py
+++ server/model/custom_ner/extract_terms.py
@@ -1,13 +1,15 @@
+import os
+
 import spacy
 from typing import List, Tuple, Any
 
-
-custom_scibert = spacy.load(R"./model-ner")
+BASE_PATH = os.path.join(os.path.dirname(__file__))
+custom_scibert = spacy.load(BASE_PATH + "/model-ner")
 
 def extract_medical_terms(text : str) -> List[str]:
     """
     Extracts medical terms from document
     """
     
-    doc = scibert(text)
+    doc = custom_scibert(text)
     return list(set([doc.ents[i].text for i in range(len(doc.ents))]))
