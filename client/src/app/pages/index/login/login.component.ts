1f75ce952dd8b28e81308a110df1637b355b6c81
PsycoCast <gabsiyassine@gmail.com>
2021-05-09 18:58:06 +0100

    add spinner to login

--- client/src/app/pages/index/login/login.component.ts
+++ client/src/app/pages/index/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../../../services/auth.service";
 import {User} from "../../../models/user";
 import {Utils} from "../../../services/utils";
+import {NgxSpinnerService} from "ngx-spinner";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-login',
@@ -10,20 +12,34 @@ import {Utils} from "../../../services/utils";
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService,
+              private spinner: NgxSpinnerService,
+  ) { }
 
   profile: User = new User();
   emailToSend = '';
   passwordToSend = '';
-
+  isLoading = false;
   validateEmail = Utils.validateEmail;
 
   ngOnInit() {
   }
 
   submitLogin(): void {
+    this.isLoading = true;
+    this.spinner.show('spinner');
     this.authService.login({username: this.emailToSend, password: this.passwordToSend}).subscribe((data) => {
       console.log(data);
+      this.isLoading = false;
+      this.spinner.hide('spinner');
+    }, (err) => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: err.error.message,
+      });
+      this.isLoading = false;
+      this.spinner.hide('spinner');
     })
   }
 
