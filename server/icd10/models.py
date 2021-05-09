7893d363b43cbe928ecb283da3ea39bf063fca03
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-05-09 03:50:32 +0100

    Add user model and admin model

--- server/icd10/models.py
+++ server/icd10/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-import uuid
+from django.contrib.auth.models import AbstractUser
+
 
 class ResearchProject(models.Model):
     STATUSES = (
@@ -11,6 +12,7 @@ class ResearchProject(models.Model):
     end_date = models.DateTimeField(auto_now_add=True, blank=True)
     status = models.CharField(max_length=30, choices=STATUSES, default='S')
     project_file_url = models.TextField(default=None, null=True)
+
 
 class ResearchItem(models.Model):
     project = models.ForeignKey(ResearchProject, related_name='items', on_delete=models.CASCADE)
@@ -38,3 +40,8 @@ class ThematicCodeItem(models.Model):
     )
     item = models.OneToOneField(ResearchItem, on_delete=models.CASCADE)
     thematic_code = models.CharField(max_length=30, choices=THEMATIC_CODES)
+
+
+class User(AbstractUser):
+    credits = models.IntegerField(default=1000)
+    image_url = models.CharField(max_length=255, null=True)
