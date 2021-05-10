9f6889e4c5af861f0b0181fa50d03f59b82417aa
PsycoCast <gabsiyassine@gmail.com>
2021-05-10 14:55:23 +0100

    get validated and predicted projects

--- client/src/app/pages/index/projects/projects.component.ts
+++ client/src/app/pages/index/projects/projects.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ProjectService} from '../../../services/project.service';
 import {NgxSpinnerService} from "ngx-spinner";
 import {ResearchProject} from "../../../models/research-project";
+import {StatisticsService} from "../../../services/statistics.service";
 
 @Component({
   selector: 'app-projects',
@@ -12,9 +13,11 @@ export class ProjectsComponent implements OnInit {
 
   isLoading = false;
   projects = new Array<ResearchProject>();
+  projectsInfos = [];
 
   constructor(private projectService: ProjectService,
               private spinner: NgxSpinnerService,
+              private statsService: StatisticsService,
   ) { }
 
   ngOnInit(): void {
@@ -27,7 +30,25 @@ export class ProjectsComponent implements OnInit {
     // this.spinner.show('spinner2');
     this.projectService.getProjects().subscribe((data) => {
       console.log(data);
+      this.projects = data;
+      this.getProjectsStatus();
     });
+  }
+
+  getProjectsStatus() {
+    this.projects.forEach((project) => {
+      this.statsService.getValidated(project.id).subscribe((validated) => {
+        this.statsService.getPredicted(project.id).subscribe((predicted) => {
+          this.projectsInfos.push({
+            id: project.id,
+            status: project.status,
+            validated: validated.percentage,
+            predicted: predicted.percentage
+          });
+          console.log(this.projectsInfos);
+        });
+      })
+    })
   }
 
 }
