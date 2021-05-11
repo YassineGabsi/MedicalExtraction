d0d0030dbc63670b65d98d0d8bb7b52c7044d8b0
PsycoCast <gabsiyassine@gmail.com>
2021-05-11 13:42:20 +0100

    fix register

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,18 +1,19 @@
 import {Injectable} from '@angular/core';
 import {GenericService} from './generic.service';
 import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {HttpBackend, HttpClient} from '@angular/common/http';
 import {Profile} from '../models/profile';
 import {User} from '../models/user';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService extends GenericService {
 
-  constructor(private http: HttpClient, private router: Router) {
+  constructor(private http: HttpClient, private router: Router, handler: HttpBackend) {
     super();
+    this.http = new HttpClient(handler);
   }
 
   public login(user: any): Observable<any> {
