af96204c10d999da984d698f0eb00d9bbbad707e
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-05-16 05:32:58 +0100

    Keep only alpha num keywords

--- server/model/custom_ner/extract_terms.py
+++ server/model/custom_ner/extract_terms.py
@@ -2,14 +2,21 @@ import os
 
 import spacy
 from typing import List, Tuple, Any
+import re
+
+
+def is_alpha_num(word: str) -> bool:
+    return re.match(r'^[\w \t]+$', word, flags=re.IGNORECASE)
+
 
 BASE_PATH = os.path.join(os.path.dirname(__file__))
 custom_scibert = spacy.load(BASE_PATH + "/model-ner")
 
-def extract_medical_terms(text : str) -> List[str]:
+
+def extract_medical_terms(text: str) -> List[str]:
     """
     Extracts medical terms from document
     """
-    
+
     doc = custom_scibert(text)
-    return list(set([doc.ents[i].text for i in range(len(doc.ents))]))
+    return list(set([doc.ents[i].text for i in range(len(doc.ents)) if is_alpha_num(doc.ents[i].text)]))
